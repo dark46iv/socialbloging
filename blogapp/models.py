74de316e115a8b9e0771a8aecbe366ba75ba473c
dark46 <dark.46.iv@yandex.ru>
2019-03-26 16:35:04 +0300

    сделал модель постов и примитивное отображение

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Post(models.Model):
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=0)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.title
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Post
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html', context={})
+
+
+class PostListView(ListView):
+
+    model = Post
+    template_name = 'index.html'
+
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super(PostListView, self).get_context_data(*args, **kwargs)
+        context['posts'] = self.model.objects.all()
+        return context
+
